a2fe1467e2e133f0fc0747e936c1f82769c48701
Muni47 <munibahmad47@gmail.com>
2023-04-26 00:17:15 +0500

    First Commit

---
+++ src/Component/SignUpInfo.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function SignUpInfo({ formData, setFormData }) {
+  return (
+    <div className="sign-up-container">
+      <input
+        type="email"
+        placeholder="Email..."
+        required
+        value={formData.email}
+        onChange={(event) =>
+          setFormData({ ...formData, email: event.target.value })
+        }
+      />
+      <input
+        type="password"
+        placeholder="Password..."
+        required
+        value={formData.password}
+        onChange={(event) =>
+          setFormData({ ...formData, password: event.target.value })
+        }
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password..."
+        required
+        value={formData.confirmPassword}
+        onChange={(event) =>
+          setFormData({ ...formData, confirmPassword: event.target.value })
+        }
+      />
+    </div>
+  );
+}
+
+export default SignUpInfo;
